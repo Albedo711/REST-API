be9bfacf26bada924617c75a2d3c099993b63271
Albedo <masfuadfotobersama19@gmail.com>
2025-02-10 17:33:49 +0700

    Sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController; 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,15 @@ use App\Http\Controllers\ProductController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+ Route::post('/logout', [AuthController::class, 'logout']);
+ Route::get('/profile', function (Request $request) {
+ return response()->json($request->user());
+ });
+});
+
 
 Route::apiResource('products', ProductController::class);
